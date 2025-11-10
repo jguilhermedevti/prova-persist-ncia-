2fa7f3b85b26a40bee29a7f83519bb176a7e6558
jguilhermedevti <jguilherme.devti@gmail.com>
2025-11-10 10:55:35 -0300

    Update arquivo01.py

--- arquivo01.py
+++ arquivo01.py
@@ -7,8 +7,34 @@ total_de_oponentes = len(nomes) - 1
 forcas_geradas = [random.randint(50, 150) for _ in range(len(nomes))]
 pokemons = pd.Series(forcas_geradas, index=nomes)
 
-print("--- Força dos Pokémons Gerada ---")
+print("Força dos Pokémons")
 print(pokemons)
-print("-----------------------------------")
 
 
+try:
+    with open("resultado_pokemons.txt", "w", encoding="utf-8") as arquivo:
+        
+        for pokemon_atual, forca_atual in pokemons.items():
+            
+            # pokemons fracos da linha
+            oponentes_vencidos = [
+                oponente 
+                for oponente, forca_oponente in pokemons.items() 
+                if pokemon_atual != oponente and forca_atual > forca_oponente
+            ]
+            
+            # resultado com base nos vencidos
+            if not oponentes_vencidos:
+                resultado = "nenhum"
+            elif len(oponentes_vencidos) == total_de_oponentes:
+                resultado = "todos"
+            else:
+                resultado = ", ".join(oponentes_vencidos)
+                
+            linha_final = f"O Pokémon {pokemon_atual}, ganha de: {resultado}\n"
+            arquivo.write(linha_final)
+            
+    print("\nArquivo 'resultado_pokemons.txt' criado com sucesso!")
+
+except Exception as e:
+    print(f"Ocorreu um erro ao escrever o arquivo: {e}")
