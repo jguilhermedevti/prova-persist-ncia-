185013a835704af704e5e212774319f28bd8e372
jguilhermedevti <jguilherme.devti@gmail.com>
2025-11-10 11:51:58 -0300

    Update questao03.py

--- questao03.py
+++ questao03.py
@@ -10,6 +10,17 @@ dados_pessoas = {
 }
 tabela_pessoas = pd.DataFrame(dados_pessoas)
 
+@app.get("/pessoas")
+def obter_pessoas():
+    # Retorna o DataFrame full em JSON
+    return tabela_pessoas.to_dict("records")
 
 
-
+@app.get("/pessoas/{cidade}")
+def obter_pessoas_por_cidade(cidade: str):
+    # Filtra a tabela pela cidade 
+    filtro = tabela_pessoas['cidade'].str.lower() == cidade.lower()
+    resultado = tabela_pessoas[filtro]
+    
+    # Retorna os dados filtrados como JSON
+    return resultado.to_dict("records")
