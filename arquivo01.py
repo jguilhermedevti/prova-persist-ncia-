e737dcf7a9442853654e67a76b5745a16670cbc2
jguilhermedevti <jguilherme.devti@gmail.com>
2025-11-10 10:53:37 -0300

    Update arquivo01.py

--- arquivo01.py
+++ arquivo01.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import random
+
+nomes = ['Pikachu', 'Charizard', 'Bulbasaur', 'Squirtle', 'Mewtwo']
+total_de_oponentes = len(nomes) - 1
+
+forcas_geradas = [random.randint(50, 150) for _ in range(len(nomes))]
+pokemons = pd.Series(forcas_geradas, index=nomes)
+
+print("--- Força dos Pokémons Gerada ---")
+print(pokemons)
+print("-----------------------------------")
+
 
